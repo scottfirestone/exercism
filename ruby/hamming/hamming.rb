36adac60c79465464906eafba74c6365e1199d2a
Scott Firestone <scott.firestone.dev@gmail.com>
2016-06-22 12:59:00 -0600

    Pass second test for hamming ruby

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,8 +1,8 @@
 class Hamming
   def self.compute(strand_one, strand_two)
     distance = 0
-    strand_one.split.each.with_index do |nucleotide, i|
-      if nucleotide == strand_two[i]
+    strand_one.chars.each.with_index do |nucleotide, i|
+      if nucleotide != strand_two[i]
         distance += 1
       end
     end
