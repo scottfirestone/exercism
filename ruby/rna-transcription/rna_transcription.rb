63282fae43742ebdcd96e4c0f5ade7e08c1720f3
Scott Firestone <scott.firestone.dev@gmail.com>
2016-06-22 13:22:41 -0600

    Pass remaining tests rna-transcription rb

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,8 +1,22 @@
 class Complement
-  def self.of_dna(nucleotide)
-    case nucleotide
-    when 'C'
-      'G'
-    end
+  def self.of_dna(strand)
+    strand.chars.map do |nucleotide|
+      case nucleotide
+      when 'C'
+        'G'
+      when 'G'
+        'C'
+      when 'T'
+        'A'
+      when 'A'
+        'U'
+      else
+        return ''
+      end
+    end.join
   end
+end
+
+class BookKeeping
+  VERSION = 4
 end
