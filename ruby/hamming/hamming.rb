460640d790f19e7b061d3039b228d8078115f770
Scott Firestone <scott.firestone.dev@gmail.com>
2016-06-22 13:06:05 -0600

    Pass all tests hamming ruby

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,5 +1,8 @@
 class Hamming
   def self.compute(strand_one, strand_two)
+    if strand_one.length != strand_two.length
+      raise ArgumentError
+    end
     distance = 0
     strand_one.chars.each.with_index do |nucleotide, i|
       if nucleotide != strand_two[i]
@@ -8,4 +11,8 @@ class Hamming
     end
     distance
   end
+end
+
+class BookKeeping
+  VERSION = 3
 end
