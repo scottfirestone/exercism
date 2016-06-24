ddd5deac10b71c9cc1cf20152c2de196216fd242
Scott Firestone <scott.firestone.dev@gmail.com>
2016-06-24 12:30:19 -0600

    Finish difference of squares rb

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -1,2 +1,25 @@
 class Squares
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
+  end
+
+  def square_of_sum
+    (0..@number).reduce(:+)**2
+  end
+
+  def sum_of_squares
+    (0..@number).reduce(0) do |result, i|
+       result + i**2
+     end
+  end
+
+  def difference
+    square_of_sum - sum_of_squares
+  end
+end
+
+class BookKeeping
+  VERSION = 3
 end
