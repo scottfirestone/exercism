29105c05c32a6499f46691db7e01e590f439e0e1
Scott Firestone <scott.firestone.dev@gmail.com>
2016-06-28 13:08:56 -0600

    Pass tests for sieve rb

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -1,2 +1,33 @@
 class Sieve
+  attr_reader :limit, :num_collection
+
+  def initialize(limit)
+    @limit = limit
+    @num_collection = {}
+  end
+
+  def primes
+    (2..limit).each do |num|
+      set_primes(num)
+    end
+    num_collection.map {|k, v| v == "prime" ? k : nil }.compact
+  end
+
+  private
+    def set_primes(num)
+      if !num_collection[num]
+        num_collection[num] = "prime"
+        set_composites(num)
+      end
+    end
+
+    def set_composites(num)
+      product = 0
+      multiplier = 2
+      until product > limit
+        product = num * multiplier
+        num_collection[product] = "composite"
+        multiplier += 1
+      end
+    end
 end
