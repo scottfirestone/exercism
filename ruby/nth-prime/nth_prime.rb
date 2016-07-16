03a21cc38ad0184544e52922e5fc1d02105ea901
Scott Firestone <scott.firestone.dev@gmail.com>
2016-07-16 09:45:50 -0600

    Add completed nth prime ruby

--- ruby/nth-prime/nth_prime.rb
+++ ruby/nth-prime/nth_prime.rb
@@ -1,2 +1,19 @@
 class Prime
+
+  def self.nth(n)
+    raise ArgumentError.new('N must be positive') if n < 1
+
+    primes ||= [2, 3]
+    candidate = primes.last
+
+    while n > primes.length
+      candidate += 2
+
+      unless primes.any? { |prime| candidate % prime == 0 }
+        primes.push(candidate)
+      end
+    end
+
+    primes[n - 1]
+  end
 end
