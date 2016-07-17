4533c0316808eb98866c4bd2e85fba11d127ebd5
Scott Firestone <scott.firestone.dev@gmail.com>
2016-07-17 12:59:09 -0600

    Pass all tests for grains ruby

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,2 +1,15 @@
 class Grains
+  def self.square(squares)
+    if squares <= 2
+      squares
+    else
+      2 ** (squares - 1)
+    end
+  end
+
+  def self.total
+    (3..64).reduce(3) do |sum, num|
+      sum += (2 ** (num - 1))
+    end
+  end
 end
