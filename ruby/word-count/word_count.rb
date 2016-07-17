7628d9dbc2c031321b53507cfa45f280b6b1cf6d
Scott Firestone <scott.firestone.dev@gmail.com>
2016-07-17 13:56:02 -0600

    Finish word count rb
    Couldn't solve apostrophes vs other punctuation

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,2 +1,14 @@
 class Phrase
+  attr_reader :phrase, :word_count
+
+  def initialize(phrase)
+    @word_count = Hash.new(0)
+    @phrase = phrase
+    arrange_words
+  end
+
+  private
+    def arrange_words
+      phrase.downcase.gsub(/\W/, ' ').split.each { |word| word_count[word] += 1 }
+    end
 end
