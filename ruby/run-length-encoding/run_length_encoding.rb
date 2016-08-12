5cf51d1d9a9334957e459017810ee627682a4a69
Scott Firestone <scott.firestone.dev@gmail.com>
2016-08-12 13:55:58 -0600

    Add psuedocode for runlengthencoding rb

--- ruby/run-length-encoding/run_length_encoding.rb
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -1,5 +1,17 @@
 class RunLengthEncodingTest
   def self.encode(input)
-    
+    # iterate through each char
+    # does the char repeat? if yes, print a number to output for number
+      # of times the char repeats
+      # then print that char
+    # does the char only appear once? if so, just print the char without a num
+  end
+
+  def self.decode(input)
+    # map through each char
+    # if the char is a number, look at the next char,
+    # and print the char x the number preceeding it
+    # keep going
+    # return output
   end
 end
