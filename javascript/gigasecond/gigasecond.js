d30faccfa267dcc49dc660f1377bd935637eb79f
Scott Firestone <scott.firestone.dev@gmail.com>
2016-09-01 12:57:57 -0600

    Complete gigasecond js

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,9 +1,9 @@
 let Gigasecond = function(date) {
-  this.startingDate = date;
+  this.startingTime = date.getTime();
 }
 
 Gigasecond.prototype.date = function() {
-
+  return new Date(this.startingTime + 1000000000000);
 }
 
 module.exports = Gigasecond;
